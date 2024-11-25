98b3baa555aa28aef6b412ebbbc1d48e89a44c2c
Badrul886 <turjame02@gmail.com>
2024-11-25 20:53:13 +0600

    successfully added the signup functionality

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ import connectMongoDB from "./db/connectMongoDB.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse from data(urlencoded)
+
 console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
@@ -13,4 +17,4 @@ app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
-})
+});
