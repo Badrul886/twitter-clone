194a861a4dce35cdf76e311c7bed2c0c0ff67364
Badrul886 <turjame02@gmail.com>
2024-11-25 22:07:46 +0600

    successfully created the getMe functionality

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse from data(urlencoded)
+
+app.use(cookieParser());
 
 console.log(process.env.MONGO_URI);
 
