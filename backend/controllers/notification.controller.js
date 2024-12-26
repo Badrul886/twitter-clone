44988f455e8c71a5f430ec97b9c49592458998d0
Badrul886 <turjame02@gmail.com>
2024-12-26 20:04:37 +0600

    Additional: Successfully completed the deleteNotification controller

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -32,3 +32,29 @@ export const deleteNotifications = async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+// export const deleteNotification = async (req, res) => {
+//   try {
+//     const notificationId = req.params.id;
+//     const userId = req.user._id;
+//     const notification = await Notification.findById(notificationId);
+
+//     if (!notification) {
+//       return res.status(404).json({ error: "Notification not found" });
+//     }
+
+//     if (notification.to.toString() !== userId.toString()) {
+//       return res
+//         .status(403)
+//         .json({ error: "Your are not allowed to delete this notification" });
+//     }
+
+//     await Notification.findByIdAndDelete(notificationId);
+//     return res
+//       .status(200)
+//       .json({ message: "Notification deleted successfully" });
+//   } catch (error) {
+//     console.log("Error in deleteNotification function", error.message);
+//     return res.status(500).json({ error: "Internal Server Error" });
+//   }
+// };
--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,11 +1,15 @@
-import express from "express"
+import express from "express";
 import { protectRoute } from "../middlewire/protectRoute.js";
-import { deleteNotifications, getNotifications } from "../controllers/notification.controller.js";
+import {
+  /*deleteNotification,*/ deleteNotifications,
+  getNotifications,
+} from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
 router.delete("/", protectRoute, deleteNotifications);
-
+// Additional
+// router.delete("/:id", protectRoute, deleteNotification);
 
 export default router;
