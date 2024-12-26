bfd34959e0144003fb2357e95fdcd219e3ae1bd2
Badrul886 <turjame02@gmail.com>
2024-12-26 19:47:55 +0600

    successfully completed the deleteNotifications controller

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -18,4 +18,17 @@ export const getNotifications = async (req, res) => {
   }
 };
 
-// export const deleteNotifications = async
+export const deleteNotifications = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({ to: userId });
+
+    return res
+      .status(200)
+      .json({ message: "Notifications deleted successfully" });
+  } catch (error) {
+    console.log("Error in deleteNotifications function", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,11 +1,11 @@
 import express from "express"
 import { protectRoute } from "../middlewire/protectRoute.js";
-import { getNotifications } from "../controllers/notification.controller.js";
+import { deleteNotifications, getNotifications } from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
-// router.delete("/", protectRoute, deleteNotifications);
+router.delete("/", protectRoute, deleteNotifications);
 
 
 export default router;
