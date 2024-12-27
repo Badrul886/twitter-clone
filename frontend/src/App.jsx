10415e8261fbd76f5c07c92d13cb02322bca072a
Badrul886 <turjame02@gmail.com>
2024-12-27 09:08:47 +0600

    Added the NotificationPage with Loading animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignUpPage from './pages/auth/signup/SignUpPage';
 import HomePage from './pages/home/HomePage';
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
       </Routes>
       <RightPanel />
     </div>
