d40403dd23178ce4c4c7612ba25f3f5f9e8eea57
Badrul886 <turjame02@gmail.com>
2024-12-27 08:18:31 +0600

    Finishing setting up the HomePage component along with the Sidebar component without functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,12 @@ import {Route, Routes} from 'react-router-dom';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import HomePage from './pages/home/HomePage';
+import Sidebar from './components/common/Sidebar';
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+      {/* Common component, bc it's not wrapped with Routes */}
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
