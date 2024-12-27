d793b649de753de8d6c02f86e42d3456a5bbdc7d
Badrul886 <turjame02@gmail.com>
2024-12-27 09:26:26 +0600

    Added the ProfilePage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,14 @@ import {Route, Routes} from 'react-router-dom';
 import LoginPage from './pages/auth/login/LoginPage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import HomePage from './pages/home/HomePage';
-import Sidebar from './components/common/Sidebar';
-import RightPanel from './components/common/RightPanel';
 import NotificationPage from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
+
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
@@ -15,6 +20,7 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignUpPage />} />
         <Route path='/notifications' element={<NotificationPage />} />
+        <Route path='/profile/:username' element={<ProfilePage />} />
       </Routes>
       <RightPanel />
     </div>
